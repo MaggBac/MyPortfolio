fae0187c53bdc1f6104b1c5dcca4caba954e0c2e
Magda <magda.bac77@gmail.com>
2022-05-29 18:18:29 +0200

    First commit

---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+const allNavItems = document.querySelectorAll('.nav-link')
+const navMobile = document.querySelector('.nav-mobile');
+const navBtn = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
+const footerYear = document.querySelector('.footer-year')
+
+const handleNav = () => {
+    navBtn.classList.toggle('is-active');
+    navMobile.classList.toggle('nav-mobile--active');
+
+    // allNavItems.forEach(item => {
+    //     item.addEventListener('click', () => {
+    //         nav.classList.remove('is-active')
+    //     })
+    // })
+
+    handleNavItemsAnimation();
+}
+
+
+
+const handleNavItemsAnimation = () =>{
+    let delayTime = 0;
+    
+    allNavItems.forEach(item => {
+        item.classList.toggle('nav-items-animation')
+        item.style.animationDelay = '.' + delayTime + 's';
+        delayTime++;
+    })
+}
+
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+
+handleCurrentYear();
+
+navBtn.addEventListener('click', handleNav);
