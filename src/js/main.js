8deb8a0a55f0d4f4912ac844cd91bc3459ec73c9
Magda <magda.bac77@gmail.com>
2022-07-07 11:18:53 +0200

    finished page

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,41 @@
-const allNavItems = document.querySelectorAll('.nav-link')
+const allNavItems = document.querySelectorAll('.nav-link');
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav');
-const footerYear = document.querySelector('.footer-year')
+const footerYear = document.querySelector('.footer-year');
 
 const handleNav = () => {
-    navBtn.classList.toggle('is-active');
-    navMobile.classList.toggle('nav-mobile--active');
+	navBtn.classList.toggle('is-active');
+	nav.classList.toggle('nav-mobile--active')
 
-    // allNavItems.forEach(item => {
-    //     item.addEventListener('click', () => {
-    //         nav.classList.remove('is-active')
-    //     })
-    // })
-
-    handleNavItemsAnimation();
-}
-
-
-
-const handleNavItemsAnimation = () =>{
-    let delayTime = 0;
-    
-    allNavItems.forEach(item => {
-        item.classList.toggle('nav-items-animation')
-        item.style.animationDelay = '.' + delayTime + 's';
-        delayTime++;
+	allNavItems.forEach(item => {
+        item.addEventListener('click', () => {
+            nav.classList.remove('nav-mobile--active')
+        })
     })
-}
+
+	allNavItems.forEach(item => {
+        item.addEventListener('click', () => {
+            navBtn.classList.remove('is-active')
+        })
+    })
+	handleNavItemsAnimation();
+};
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach((item) => {
+		item.classList.toggle('nav-items-animation');
+		item.style.animationDelay = '.' + delayTime + 's';
+		delayTime++;
+	});
+};
 
 const handleCurrentYear = () => {
-    const year = (new Date).getFullYear();
-    footerYear.innerText = year;
-}
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
 
 handleCurrentYear();
 
